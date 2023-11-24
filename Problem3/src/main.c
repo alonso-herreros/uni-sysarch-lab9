6c9626a95a453f79b9be0c873da97001b59efe14
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-11-24 12:15:46 +0100

    Solve section 3.2

--- Problem3/src/main.c
+++ Problem3/src/main.c
@@ -11,7 +11,11 @@ const char *DWARVES[N_DWARVES] = {"Doc", "Grumpy", "Happy", "Sleepy", "Bashful",
 
 // Declarations
 void Process_dwarf(pid_t ppid, const char *name);
-void Process_Snowwhite();
+void Process_Snowwhite(pid_t pids[N_DWARVES]);
+void SW_sighandler(int sig);
+
+
+static volatile int sw_waiting;
 
 
 // MAIN
@@ -24,5 +28,29 @@ int main(int argc, char **argv)
         else if (pids[i] == 0)  Process_dwarf(ppid, DWARVES[i]);
     }
 
-    Process_Snowwhite();
+    Process_Snowwhite(pids);
+}
+
+
+void Process_Snowwhite(pid_t pids[N_DWARVES])
+{
+    signal(SIGUSR1, SW_sighandler);
+
+    for (int i=0; i<N_DWARVES; i++) {
+        printf("HI,HO!! %s\n", DWARVES[i]);
+        sw_waiting = 1;
+        kill(pids[i], SIGUSR1);
+        
+        while(sw_waiting);
+    }
+
+    exit(EXIT_SUCCESS);
+}
+
+
+void SW_sighandler(int sig)
+{
+    if (sig == SIGUSR1) {
+        sw_waiting = 0;
+    }
 }
