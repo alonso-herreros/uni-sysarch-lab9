1efca32a682470063c9788db3db9725d3c334607
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-11-24 12:15:55 +0100

    Solve section 3.3

--- Problem3/src/main.c
+++ Problem3/src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <signal.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -11,11 +12,13 @@ const char *DWARVES[N_DWARVES] = {"Doc", "Grumpy", "Happy", "Sleepy", "Bashful",
 
 // Declarations
 void Process_dwarf(pid_t ppid, const char *name);
+void DW_sighandler(int sig);
 void Process_Snowwhite(pid_t pids[N_DWARVES]);
 void SW_sighandler(int sig);
 
 
 static volatile int sw_waiting;
+static char *dwarf_name;
 
 
 // MAIN
@@ -27,8 +30,29 @@ int main(int argc, char **argv)
         if ((pids[i] = fork()) < 0)  exit(EXIT_FAILURE);
         else if (pids[i] == 0)  Process_dwarf(ppid, DWARVES[i]);
     }
+    sleep(1);
 
     Process_Snowwhite(pids);
+}
+
+
+void Process_dwarf(pid_t ppid, const char *name)
+{
+    signal(SIGUSR1, DW_sighandler);
+
+    dwarf_name = strdup(name);
+    printf("HELLO I am %s, my pid is %d and my father is %d\n", name, getpid(), ppid);
+    while (1);
+}
+
+void DW_sighandler(int sig)
+{
+    if (sig == SIGUSR1) {
+        printf("%s HI,HO!!\n", dwarf_name);
+        kill(getppid(), SIGUSR1);
+        free(dwarf_name);
+        exit(EXIT_SUCCESS);
+    }
 }
 
 
